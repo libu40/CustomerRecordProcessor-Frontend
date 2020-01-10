0c59f21a638a6323ca06fc2465d0a5d3018af06f
lmathew <lmathew@qualogy.com>
2020-01-10 12:32:45 +0100

    Introducing the nav menu changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,46 @@
+import { UploadComponent } from './upload/upload.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from  '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {RouterModule, Routes} from "@angular/router";
+import {CustomMaterialModule} from "./core/material.module";
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HomeComponent} from './home/home.component';
-import {UploadComponent} from './upload/upload.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent, data: { title: 'Home Component' } },
+    { path: 'home', component: HomeComponent, data: { title: 'Home Component' } },
+    { path: 'upload', component: UploadComponent, data: { title: 'Upload Component' } },
+    { path: 'about-us', component: AboutUsComponent, data: { title: 'About us Component' } }
+  ];
+
+   
 @NgModule({
-  declarations: [
+    declarations: [
     AppComponent,
     HomeComponent,
-    UploadComponent
-  ],
-  imports: [
+    UploadComponent,
+    AboutUsComponent
+    ],
+    imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+        appRoutes,
+      ),
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    CustomMaterialModule
+    ],
+    providers:[],
+    bootstrap: [AppComponent]
+
 })
-export class AppModule { }
+
+export class  AppModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 }
 
 )
-export class HomeComponent  {
+export class HomeComponent implements OnInit  {
+    ngOnInit(): void {
+        
+    }
     
 
 }
